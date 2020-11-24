d73e849496e1e262be67f3a8f77894c0cc9839d4
Adarsh_Menon <adarsh.menon@fusionsport.com>
2020-11-25 08:19:04 +1000

    Implemented the outer facing contract

---
+++ CourseLibrary.API/Controllers/AuthorsControllers.cs
@@ -0,0 +1,40 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    // The basics that should be done while implementing the outer-facing contract for the API
+    // 1. Controller
+    // 2. Inject repository
+    // 3. Create action methods
+    // 4. Return results.
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsControllers : ControllerBase 
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsControllers(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId:guid}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            return new JsonResult(authorFromRepo);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # courselibrary-resful-api-.net-core3
 
-Build a RESTful API with ASP.NET Core 3
-1. Interact with your API using the correct HTTP methods and status code, and what method safety and idempotency are all about.
+Build a RESTful API with ASP.NET Core 3. Key focus areas are:
+1. Interact with the API using the correct HTTP methods and status code, and what method safety and idempotency are all about.
 2. Validate input that’s sent to the API, and how to implement common features like filtering and searching
