b0d47d2cb00f37b5c9fa2d1baa3ce646c62cbcc9
Adarsh_Menon <adarsh.menon@fusionsport.com>
2020-11-25 10:08:37 +1000

    Working with content negotiation and outut formatters

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -3,6 +3,7 @@ using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,7 +26,17 @@ namespace CourseLibrary.API
         // This method is used to add services to the built-in dependency injection container and to configure those services.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers();
+            // Adds framework services used when building APIs to the container so they can be sued for dependency injection.
+            // AddControllers() method accepts an action to configure.
+            // We will configure this service to return
+            services.AddControllers(setupAction => 
+            {
+                setupAction.ReturnHttpNotAcceptable = true; // Default value is false, which will return the default media type (e.g JSON) when requested.
+
+                // Adding steps like these will support additional format for output.
+                //setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+
+            }).AddXmlDataContractSerializerFormatters(); // Support XML format for output. If not added will give a 406 (Not Acceptable) error. This is the preferred way for adding formatters.
              
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
