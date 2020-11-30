d198c942d89e205373afae45fdbba88560162bb0
Adarsh_Menon <adarsh.menon@fusionsport.com>
2020-12-01 09:09:19 +1000

    Class level input validation with IValidatableObject for child resource.

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto : IValidatableObject // implementing this interface provides a way for an object to be invalidated in any way we want.
     {
         [Required]
         [MaxLength(100)]
@@ -14,5 +14,15 @@ namespace CourseLibrary.API.Models
 
         [MaxLength(1500)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                // yield return will make sure the validation result is immediately returned after which the code execution will continue. 
+                // ValidationResult is an object that is used to provide error messages and relay the property's member or model names.
+                yield return new ValidationResult("The provided description should be different from the title.", new[] {"CourseForCreationDto"});
+            }
+        }
     }
 }
