44ff87ba1ba32f0b3462213ecfa2063c40171bca
Adarsh_Menon <adarsh.menon@fusionsport.com>
2020-12-01 09:42:17 +1000

    Class level input validation with a custom attribute.

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto : IValidatableObject // implementing this interface provides a way for an object to be invalidated in any way we want.
+    [CourseTitleMustBeDifferentFromDescription]
+    public class CourseForCreationDto // : IValidatableObject // implementing this interface provides a way for an object to be invalidated in any way we want.
     {
         [Required]
         [MaxLength(100)]
@@ -15,14 +17,14 @@ namespace CourseLibrary.API.Models
         [MaxLength(1500)]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                // yield return will make sure the validation result is immediately returned after which the code execution will continue. 
-                // ValidationResult is an object that is used to provide error messages and relay the property's member or model names.
-                yield return new ValidationResult("The provided description should be different from the title.", new[] {"CourseForCreationDto"});
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        // yield return will make sure the validation result is immediately returned after which the code execution will continue. 
+        //        // ValidationResult is an object that is used to provide error messages and relay the property's member or model names.
+        //        yield return new ValidationResult("The provided description should be different from the title.", new[] {"CourseForCreationDto"});
+        //    }
+        //}
     }
 }
