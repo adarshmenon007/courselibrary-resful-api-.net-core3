f9f14b6df859a796577748a4ce75d0f8e6a741d5
Adarsh_Menon <adarsh.menon@fusionsport.com>
2020-11-30 12:17:38 +1000

    Returning 201 Created status code with the collection on newly created Authors in the header after creating a collection of parent resource (Author).

--- CourseLibrary.API/Controllers/AuthorCollectionsController.cs
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +24,28 @@ namespace CourseLibrary.API.Controllers
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
+        [HttpGet("({ids})", Name = "GetAuthorCollection")]
+        public IActionResult GetAuthorCollection(
+        [FromRoute]
+        [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = _courseLibraryRepository.GetAuthors(ids);
+
+            if (ids.Count() != authorEntities.Count())
+            {
+                return NotFound();
+            }
+
+            var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+
+            return Ok(authorsToReturn);
+        }
+
         public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorCollection)
         {
             var authorEntities = _mapper.Map<IEnumerable<Entities.Author>>(authorCollection);
@@ -34,9 +57,10 @@ namespace CourseLibrary.API.Controllers
 
             _courseLibraryRepository.Save();
 
-            return Ok();
+            var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
 
+            return CreatedAtRoute("GetAuthorCollection", new { ids = idsAsString }, authorCollectionToReturn);
         }
-
     }
 }
